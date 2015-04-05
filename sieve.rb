4f7127d3f22c76e7f3a9925184ad45d297c2fa98
Tony DiNitto <think41c@gmail.com>
2015-04-05 00:43:35 -0600

    Initial framework set. Hypothesis on prime number established in pseudocode.

--- sieve.rb
+++ sieve.rb
@@ -1,11 +1,21 @@
 class Sieve
   
+  # attr_reader :num_of_primes
+
   def initialize(num_of_primes)
     @num_of_primes = num_of_primes
   end
 
   def primes
-    [2,3,5,7]
+    primes_found = []
+    current_num  = 1
+
+    until primes_found.length == @num_of_primes
+      # Hypothesis: Is a number divisible by 2, 3, 5, 7? Then it's not prime.
+      # 13 ? Prime.
+      # 17 ? Prime.
+      # 23 ? Prime. 
+    end
   end
 
 end
