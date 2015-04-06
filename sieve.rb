8fda2d848dd33a8a8f76138acb47fdf5af6fff10
Tony DiNitto <think41c@gmail.com>
2015-04-05 23:48:10 -0600

    Ultra fast calculations due to refactor prime calculating method.

--- sieve.rb
+++ sieve.rb
@@ -7,26 +7,22 @@ class Sieve
 
   def prime?(num)
     upper_range = num/2
-    (2..upper_range).none? { |x| num % x == 0 }
-    # Refactor hypothesis : To see if a number is prime, it must be divisible by other prime numbers that
-    # are smaller than it only. So don't check number to divide by, but only the current array of prime 
-    # numbers. This will require major logic change/refactoring. 
-    # I think an .any enum would work. 
+    @primes_found.none? { |x| num % x == 0 }
   end
 
   def primes
     current_num  = 3
-    primes_found = [2]
+    @primes_found = [2]
     
     until current_num == @num_of_primes
       if prime?(current_num)
-        primes_found << current_num
+        @primes_found << current_num
         current_num += 1 
       else
         current_num += 1 
       end
     end
-    primes_found
+    @primes_found
   end
 end
 
