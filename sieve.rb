f5d906e010bd07e124b4545375650a37eb7dd38d
Tony DiNitto <think41c@gmail.com>
2015-04-09 14:57:34 -0600

    Logic doesn't check for numbers divisible by 2. Speeds up further.

--- sieve.rb
+++ sieve.rb
@@ -17,15 +17,19 @@ class Sieve
   end
 
   def primes
-    current_num  = 3
+    current_num   = 3
+    divide_into   = 3
     @primes_found = [2]
     
     until current_num == @num_primes_to_find
-      if prime?(current_num)
-        @primes_found << current_num
+      if prime?(divide_into)
+        @primes_found << divide_into
       end
       current_num += 1
+      divide_into += 2
+      return @primes_found if divide_into > @num_primes_to_find
     end
-    @primes_found
   end
 end
+# a = Sieve.new(10)
+# p a.primes
